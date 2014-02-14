37343e2da0901f3071d0864e35ff71cbc9a61c43
Jni <jni.viens@gmail.com>
2014-02-14 16:26:32 -0500

    Fixed default MVC route

--- CleanCart.Web/App_Start/RouteConfig.cs
+++ CleanCart.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CleanCart
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ShopCatalog", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
