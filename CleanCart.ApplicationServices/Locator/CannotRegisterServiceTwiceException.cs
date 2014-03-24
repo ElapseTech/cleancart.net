5445e83916a3970285e52ae3f7d11c23d6a9d8f9
Jni <jni.viens@gmail.com>
2014-03-24 15:05:22 -0400

    Mark exceptions as serialisable

--- CleanCart.ApplicationServices/Locator/CannotRegisterServiceTwiceException.cs
+++ CleanCart.ApplicationServices/Locator/CannotRegisterServiceTwiceException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace CleanCart.ApplicationServices.Locator
 {
+    [Serializable]
     public class CannotRegisterServiceTwiceException : Exception
     {
         public CannotRegisterServiceTwiceException(Type serviceType) :
