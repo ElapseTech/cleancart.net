2f62f8ff4975e213fc848d6eaa34730759718c31
jni- <jni.viens@gmail.com>
2014-03-25 11:06:16 -0700

    Added a helper to get form field names

--- CleanCart.AcceptanceTests/Fixtures/AddShopCatalogFixture.cs
+++ CleanCart.AcceptanceTests/Fixtures/AddShopCatalogFixture.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using CleanCart.AcceptanceTests.Helpers;
+using CleanCart.ApplicationServices.Dto;
 using OpenQA.Selenium;
 
 namespace CleanCart.AcceptanceTests.Fixtures
 {
     class AddShopCatalogFixture : CleanCartWebFixture
     {
+        private readonly FormHelper<CatalogItemDTO> _formHelper = new FormHelper<CatalogItemDTO>();
+ 
         public void NavigateToForm()
         {
             Driver.Navigate().GoToUrl(CreateAbsoluteUrl());
@@ -12,12 +16,12 @@ namespace CleanCart.AcceptanceTests.Fixtures
 
         public void FillInItemCode(string itemCode)
         {
-            Driver.FindElement(By.Name("CodeText")).SendKeys(itemCode);
+            Driver.FindElement(By.Name(_formHelper.FieldName(x => x.CodeText))).SendKeys(itemCode);
         }
 
         public void FillInTitle(string itemTitle)
         {
-            Driver.FindElement(By.Name("Title")).SendKeys(itemTitle);
+            Driver.FindElement(By.Name(_formHelper.FieldName(x => x.Title))).SendKeys(itemTitle);
         }
 
         public void SubmitForm()
---
+++ CleanCart.AcceptanceTests/Helpers/FormHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CleanCart.AcceptanceTests.Helpers
+{
+    class FormHelper<TForm>
+    {
+        public string FieldName<TField>(Expression<Func<TForm, TField>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("You must use a simple expression like 'x => x.Field' to be able to retrieve the field name");
+            }
+
+            return memberExpression.Member.Name;
+        }
+    }
+}
