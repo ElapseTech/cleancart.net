e09ed0e243e2dd4ff7206c2cda481e733d54c3d3
FA Bourbonnais <fbourbonnais@elapsetech.com>
2014-03-27 16:45:59 -0400

    Introduce an Empty In Meomory context

---
+++ CleanCart.ConfigurationContexts/EmtyInMemoryContext.cs
@@ -0,0 +1,22 @@
+﻿using CleanCart.ApplicationServices.Locator;
+using CleanCart.ConfigurationContexts.Configurations.Persistence;
+using CleanCart.ConfigurationContexts.Fillers.ShopCatalog;
+
+namespace CleanCart.ConfigurationContexts
+{
+    public class EmptyInMemoryContext : ContextBase
+    {
+
+        public void Apply()
+        {
+            Prepare();
+
+            UseConfiguration<InMemoryPersistenceConfiguration>();
+        }
+
+        private void Prepare()
+        {
+            ServiceLocator.Locator.Reset();
+        }
+    }
+}
