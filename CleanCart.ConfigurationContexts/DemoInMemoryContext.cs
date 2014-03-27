05e9bece0b98bd41a43a9cd7507952d18b81f661
FA Bourbonnais <fbourbonnais@elapsetech.com>
2014-03-27 16:19:53 -0400

    Reset the Service Locator before applying a new Context

--- CleanCart.ConfigurationContexts/DemoInMemoryContext.cs
+++ CleanCart.ConfigurationContexts/DemoInMemoryContext.cs
@@ -1,15 +1,23 @@
-﻿using CleanCart.ConfigurationContexts.Configurations.Persistence;
+﻿using CleanCart.ApplicationServices.Locator;
+using CleanCart.ConfigurationContexts.Configurations.Persistence;
 using CleanCart.ConfigurationContexts.Fillers.ShopCatalog;
 
 namespace CleanCart.ConfigurationContexts
 {
     public class DemoInMemoryContext : ContextBase
     {
+
         public void Apply()
         {
+            Prepare();
+
             UseConfiguration<InMemoryPersistenceConfiguration>();
-            
             UseFiller<InMemoryShopCatalogFiller>();
+        }
+
+        private void Prepare()
+        {
+            ServiceLocator.Locator.Reset();
         }
     }
 }
